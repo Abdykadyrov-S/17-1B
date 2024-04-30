0f3ae0d2d2849b6e081892a68f6fce924c0352b0
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-04-30 18:29:51 +0600

    create test.py

--- lesson_5.py
+++ lesson_5.py
@@ -10,9 +10,31 @@ git config --global user.email "YOUR EMAIL ON GITHUB"
 
 
 """
+
+Команды для пуша на новый репозиторий 
+
 git init
 
-git add lesson_1.py 
+
+git add .
+ # git add lesson_1.py 
+
+git commit -m "first commit"
+ # git commit -m "сохранил код второго месяца обучения"
+
+git branch -M main 
+
+git remote add origin <HTTPS link.git>
+ # git remote add origin https://github.com/Abdykadyrov-S/17-1B.git
+
+git push -u origin main
+
+
+git status
+
+"""
+
+"""
 
 git add .
 
